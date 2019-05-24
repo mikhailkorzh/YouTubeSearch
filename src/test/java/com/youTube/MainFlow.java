5d376e70ac11ffec15c14d6508c45494ca5136d9
mikhailkorzh <mikhail.korzh@gmail.com>
2019-05-24 10:42:24 +0300

    Update MainFlow.java
    Some imported packages were removed

--- src/test/java/com/youTube/MainFlow.java
+++ src/test/java/com/youTube/MainFlow.java
@@ -2,15 +2,12 @@ package com.youTube;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
+//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
